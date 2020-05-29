51909202cfe70b9731040b8085c5bbd01f4c83bc
rocket049 <fuhuizn@163.com>
2020-05-29 14:11:34 +0800

    initial.

---
+++ examples/beep/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "gitee.com/rocket049/go-beep"
+
+func main() {
+	player, err := beep.NewBeepPlayer()
+	if err != nil {
+		panic(err)
+	}
+	defer player.Close()
+	snds := []int{700, 1000, 1500, 2000}
+	for _, v := range snds {
+		player.Beep(v, 300)
+	}
+}
